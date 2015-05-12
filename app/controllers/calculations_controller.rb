8a33525a16baa75ba89b275f5e4a904b84f44349
snazaran <snazaran@gmail.com>
2015-05-11 19:53:13 -0500

    finish

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,33 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+    @input = params["num"]
+    @square = @input.to_i ** 2
+  end
+
+
   def sqrt
+    @input = params["num"]
+    @sqrt = @input.to_i ** 0.5
+    render "square_root"
   end
 
   def pmt
+
+    @rate = params["rate"].to_f
+    @nper = params["nper"].to_i
+    @pv = params["pv"].to_f
+
+@rate_decimal = (@rate/12)/100
+    @rate_factor = 1+((@rate/12)/100)
+   @fv_factor= @rate_factor**@nper
+   @multiplier = @fv_factor/(@fv_factor - 1)
+
+
+
+    @pmt = @pv * @rate_decimal * @multiplier
+    @payment = @pmt.round(2)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:num", { :controller => "calculations", :action => "sqrt" })
+  get("/square/:num",      { :controller => "calculations", :action => "square"})
+
+
+  get("/payment/:rate/:nper/:pv", { :controller => "calculations", :action => "pmt"})
 end
